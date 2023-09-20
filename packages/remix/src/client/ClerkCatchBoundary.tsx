4050878deea31ae3edc5ca2528c467cccf530db1
Carina Koo <carinakoo@gmail.com>
2023-09-20 12:11:00 -0400

    upgrade to remix 2.0 and publish fork as custom package @datairis/clerk-remix

--- packages/remix/src/client/ClerkCatchBoundary.tsx
+++ packages/remix/src/client/ClerkCatchBoundary.tsx
@@ -1,12 +1,17 @@
-import { useCatch } from '@remix-run/react';
+import { isRouteErrorResponse, useRouteError } from '@remix-run/react';
 import React from 'react';
 
 import { Interstitial } from './Interstitial';
 
 export function ClerkCatchBoundary(RootCatchBoundary?: React.ComponentType) {
   return () => {
-    const { data } = useCatch();
-    const { __clerk_ssr_interstitial_html } = data?.clerkState?.__internal_clerk_state || {};
+    const error = useRouteError();
+
+    let __clerk_ssr_interstitial_html;
+
+    if (isRouteErrorResponse(error)) {
+      ({ __clerk_ssr_interstitial_html } = error?.data?.clerkState?.__internal_clerk_state || {});
+    }
 
     if (__clerk_ssr_interstitial_html) {
       return <Interstitial html={__clerk_ssr_interstitial_html} />;
